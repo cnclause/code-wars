cb8a50a68676d05b7129b0b5015b597be43de8c3
github username <github email address>
2020-02-07 09:39:20 -0700

    added different solution with filter

--- javascript/arrayDiff.js
+++ javascript/arrayDiff.js
@@ -7,29 +7,39 @@
 
 // arrayDiff([1,2,2,2,3],[2]) == [1,3]
 
-a =["cat", "dog"]
-b = ["dog"]
+a =[17,2,-8,-13,-7,-20,0,-11]
+b = [-11,17,-20,2,-8]
 
-function arrayDiff(a, b){
+// function arrayDiff(a, b){
 
-  let repeatedElement = []
+//   let repeatedElement = []
 
-  for(let element of b){
-    if(a.includes(element)){
-      repeatedElement.push(element)
-    }
+//   for(let element of b){
+//     if(a.includes(element)){
+//       repeatedElement.push(element)
+//     }
+//   }
+//   if(repeatedElement.length >= 1){
+//     for(let e in repeatedElement){
+//      let elementinA = a.find(element => element == repeatedElement[e])
+
+//      let index = a.indexOf(elementinA)
+//      if(index > -1){
+//        a.splice(index, 1)
+//      }
+//     }
+//   }
+//   console.log(a)
+// }
+
+function arrayDiff(a, b) {
+    // Use array filter to check if every num in array a can be found in array b
+    return a.filter(num => {
+      // If num is not found in array b, return num
+      if (!b.includes(num)) {
+        console.log(num);
+      }
+    });
   }
-  if(repeatedElement.length >= 1){
-    for(let e in repeatedElement){
-     let elementinA = a.find(element => element == repeatedElement[e])
-
-     let index = a.indexOf(elementinA)
-     if(index > -1){
-       a.splice(index, 1)
-     }
-    }
-  }
-  console.log(a)
-}
 
 arrayDiff(a, b)
